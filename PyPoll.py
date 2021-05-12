766fc74d96ce59b0cf40ae5bceb3dbe871ffbcc0
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-12 18:14:58 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -4,10 +4,9 @@
 # The percentage of votes each candidate won
 # The winner of the election based on popular vote
 
-#Import Modules
+#Import Dependencies
 import os
 import csv
-
 
 #Assign a variable for the file to load and the path
 file_to_load = os.path.join("Resources", "election_results.csv")
@@ -15,14 +14,35 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign a variable for the file to save to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
+# Initialize a total vote counter
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis.
+    # Perform analysis
 
-    # Read the file object with the reader function.
+    # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Skip the header row
     headers = next(file_reader)
-    print(headers)
+    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            #Add the candidate to the candidate list
+            candidate_options.append(candidate_name)
+        
+# Print the candidate list
+print(candidate_options)
