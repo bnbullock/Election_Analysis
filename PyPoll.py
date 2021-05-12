e0d120eee2f84ab292d0b7bc2715ab472a9c56a5
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-12 17:18:24 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -17,3 +17,18 @@ with open(file_to_load) as election_data:
 
     # To do: perform analysis.
     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+    # txt_file.write("Arapahoe, ")
+    # txt_file.write("Denver, ")
+    # txt_file.write("Jefferson")
+
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("------------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
