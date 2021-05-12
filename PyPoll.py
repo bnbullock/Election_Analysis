3220a835e907d1da8d7f4f78cc6019c913e97b99
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-12 13:51:08 -0400

    Update of Project

---
+++ PyPoll.py
@@ -0,0 +1,16 @@
+# The data we need to retrieve
+# The Total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The winner of the election based on popular vote
+
+
+#Assign a variable for the file to load and the path
+file_to_load = 'Resources/election_results.csv'
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: perform analysis.
+    print(election_data)
+
