5de53b4b206ff87c0ae7672d50016b7c735b81bd
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-13 04:04:19 -0400

    Outputing to a txt file.

--- PyPoll.py
+++ PyPoll.py
@@ -60,26 +60,41 @@ with open(file_to_load) as election_data:
 
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
+
+#Save the results to our text file
+with open(file_to_save, "w") as txt_file:
         
     # Calculate percentage of votes per candidate
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
-        vote_percentage = float(votes) / float(total_votes)* 100
-     
+        vote_percentage = float(votes) / float(total_votes)* 100         
 
         # Print out each candidate's name, vote count, and percentage of
         # votes to the terminal
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
         if(votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
 
-    winning_candidate_summary = (
+
+        winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+
+        # print(winning_candidate_summary)
+
+    # Print the final vote count to the terminal.
+    election_results = (
+    f"\nElection Results\n"
     f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"Total Votes: {total_votes:,}\n"
     f"-------------------------\n")
-    print(winning_candidate_summary)
+    print(election_results, end="")
+
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
