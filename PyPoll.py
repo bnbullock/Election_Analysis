5f4d6d9afb7339d29f9a644bfe5ab870416c039b
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-05-13 03:22:53 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -65,14 +65,24 @@ with open(file_to_load) as election_data:
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes)* 100
-        # print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+     
 
         # Print out each candidate's name, vote count, and percentage of
         # votes to the terminal
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
         if(votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 # Print the candidate vote dictionary
 # print(candidate_votes)
